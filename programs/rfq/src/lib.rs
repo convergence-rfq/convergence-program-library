d5e8ec37925f690af5a68be587b86c9c70b578f2
Vitaly Shatkovsky <vitalys@Vitalys-MBP.fios-router.home>
2022-03-05 20:03:01 -0500

    added Uwe's test, added naive place limit order and naive test for it

--- programs/rfq/src/lib.rs
+++ programs/rfq/src/lib.rs
@@ -8,9 +8,29 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod rfq {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    /// Initializes protocol.
+    ///
+    /// fee_denominator Fee denominator
+    /// fee_numerator Fee numerator
+    /// ctx Accounts context
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        fee_denominator: u64,
+        fee_numerator: u64,
+    ) -> ProgramResult {
+        let protocol = &mut ctx.accounts.protocol;
+        protocol.access_manager_count = 0;
+        protocol.rfq_count = 0;
+        protocol.authority = ctx.accounts.authority.key();
+        protocol.fee_denominator = fee_denominator;
+        protocol.fee_numerator = fee_numerator;
         Ok(())
     }
+
+    /// a PDA is the authority a given limit order book
+    /// a PDA is the authority for a given RFQ state
+    /// when limit order is placed, it gets transferred to an Escrow-style PDA, and when that order is matched, the PDA transfers tokens
+    /// limit order book is 3 vectors: bids, asks, market
 
     pub fn initialize_RFQ(
         ctx: Context<InitializeRFQ>,
@@ -28,7 +48,11 @@ pub mod rfq {
         rfq_state.strike = strike;
         rfq_state.ratio = ratio;
         rfq_state.n_of_legs = n_of_legs;
-        
+
+        let order_book_state = &mut ctx.accounts.order_book_state;
+        order_book_state.bids = vec![];
+        order_book_state.asks = vec![];
+
         Ok(())
     }
 
@@ -37,6 +61,21 @@ pub mod rfq {
         action: bool,
         price: u64,
     ) -> ProgramResult {
+        let rfq_state = &mut ctx.accounts.rfq_state;
+        let order_book_state = &mut ctx.accounts.order_book_state;
+        let mut bids = order_book_state.bids.clone();
+        let mut asks = order_book_state.asks.clone();
+
+        if action == true {
+            bids.push(price);
+            bids.sort();
+        } else {
+            asks.push(price);
+            asks.sort();
+        }
+
+        order_book_state.bids = bids;
+        order_book_state.asks = asks;
 
         Ok(())
     }
@@ -52,37 +91,79 @@ pub mod rfq {
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     pub authority: Signer<'info>,
-    pub state: Account<'info, State>,
+    #[account(
+        init,
+        payer = authority,
+        seeds = [b"convergence_rfq"],
+        space = 8 + 32 + 8 + 8 + 8 + 8,
+        bump
+    )]
+    pub protocol: Account<'info, GlobalState>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct PlaceLimitOrder<'info> {
-    pub authority: Signer<'info>,
-    pub rfq_state: Account<'info, RFQState>,
-    pub order_book: Account<'info, OrderBook>
-}
-
-#[derive(Accounts)]
-#[instruction(
-    action: bool, //buy or sell
-    instrument: u8, // Token, Future, Option, may not be needed
-    rfq_expiry: i64, // when this RFQ timer expires
-    strike: u64, // if an option, else inert
-    ratio: u8, // 1, inert for now, will be relevant for multi leg
-    n_of_legs: u8, // inert for now
-)]
 pub struct InitializeRFQ<'info> {
     pub authority: Signer<'info>,
     
-    pub token: Account<'info, TokenAccount>,
-    pub quote_token: Account<'info, TokenAccount>,
-    pub rfq_state: Account<'info, RFQState>,
-    pub global_state: Account<'info, State>,
+    // tokens
+    //pub asset_token: Account<'info, TokenAccount>,
+    //pub quote_token: Account<'info, TokenAccount>,
 
+    #[account(
+        init,
+        payer = authority,
+        seeds = [b"rfq_state"],
+        space = 1024,
+        bump
+    )]
+    pub rfq_state: Account<'info, RFQState>,
+    #[account(
+        init,
+        payer = authority,
+        seeds = [b"order_book_state"],
+        space = 1024,
+        bump
+    )]
+    pub order_book_state : Account<'info, OrderBookState>,
+    #[account(
+        init,
+        payer = authority,
+        seeds = [b"convergence_rfq"],
+        space = 8 + 32 + 8 + 8 + 8 + 8,
+        bump
+    )]
+    pub global_state: Account<'info, GlobalState>,
+    //
+    // programs
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>,
     pub rent: Sysvar<'info, Rent>,
+}
+
+
+#[derive(Accounts)]
+pub struct PlaceLimitOrder<'info> {
+    pub authority: Signer<'info>,
+    #[account(
+        init,
+        payer = authority,
+        seeds = [b"rfq_state"],
+        space = 1024,
+        bump
+    )]
+    pub rfq_state: Account<'info, RFQState>,
+    #[account(
+        init,
+        payer = authority,
+        seeds = [b"order_book_state"],
+        space = 1024,
+        bump
+    )]
+    pub order_book_state: Account<'info, OrderBookState>,
+    
+    pub system_program: Program<'info, System>,
 }
 
 /// holds state of an RFQ, TODO: add internal order book logic
@@ -91,19 +172,29 @@ pub struct RFQState {
     pub action: bool,
     pub instrument: u8,
     pub rfq_expiry: i64,
+    pub expiry: i64,
     pub strike: u64,
     pub ratio: u8,
     pub n_of_legs: u8,
-    pub best_offer: u64,
     pub best_bid: u64,
+    pub best_offer: u64,
 }
 
 #[account]
-pub struct OrderBook {
+pub struct OrderBookState { 
+    pub bids: Vec<u64>,
+    pub asks: Vec<u64>,
+    // rfq_state: RFQState,
 }
 
 /// global state for RFQ system
 #[account]
-pub struct State { }
+pub struct GlobalState {
+    pub rfq_count: u64, // ? for our indexooors
+    pub access_manager_count: u64,
+    pub authority: Pubkey,
+    pub fee_denominator: u64,
+    pub fee_numerator: u64,
+}
 
 
--- tests/rfq.ts
+++ tests/rfq.ts
@@ -1,17 +1,135 @@
 import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
+import { Program, Provider } from '@project-serum/anchor';
 import { Rfq } from '../target/types/rfq';
+import * as assert from 'assert';
+import * as idl from '../target/idl/rfq.json';
+import * as spl from "@solana/spl-token";
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  Signer,
+  SystemProgram,
+  SYSVAR_RENT_PUBKEY,
+  Transaction,
+  TransactionInstruction,
+} from "@solana/web3.js";
 
 describe('rfq', () => {
 
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
-
+  const provider = anchor.getProvider();
   const program = anchor.workspace.Rfq as Program<Rfq>;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  it('Initializes RFQs', async () => {
+    const feeDenominator = 1_000;
+    const feeNumerator = 3;
+    const { tx, state } = await initializeProtocol(provider, feeDenominator, feeNumerator);
+    assert.ok(state.rfqCount.eq(new anchor.BN(0)));
+    console.log("Your transaction signature");
+
   });
+
+  it('Initializes new RFQ', async () => {
+
+  })
+
+  it('Places new limit orders', async () => {
+
+  })
+
+  
+
 });
+
+
+export async function initializeRFQ(
+  provider: Provider,
+  action: boolean,
+  instrument: number,
+  rfqExpiry: anchor.BN,
+  strike: anchor.BN,
+  ratio: number,
+  nOfLegs, number
+): Promise<any> {
+  
+  const program = await getProgram(provider);
+  console.log("program", program.programId);
+  const [globalPDA, _globalBump] = await getPda(provider, 'convergence_rfq');
+  const [rfqPDA, _rfqBump] = await getPda(provider, 'rfq_state');
+  const [orderBookPDA, _orderBookBump] = await getPda(provider, 'order_book_state');
+
+  const tx = await program.rpc.initializeRfq(
+    action, 
+    instrument,
+    rfqExpiry,
+    strike,
+    ratio,
+    nOfLegs,
+  {
+    accounts: {
+      authority: provider.wallet.publicKey,
+      rfqState: rfqPDA,
+      orderBookState:orderBookPDA,
+      globalState: globalPDA,
+      systemProgram: anchor.web3.SystemProgram.programId,
+      tokenProgram: spl.TOKEN_PROGRAM_ID,
+      associatedTokenProgram: spl.ASSOCIATED_TOKEN_PROGRAM_ID,
+      rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      
+    }
+  });
+
+  const state = await program.account.globalState.fetch(globalPDA)
+  return { tx, state };
+}
+
+export async function initializeProtocol(
+  provider: Provider,
+  feeDenominator: number,
+  feeNumerator: number
+): Promise<any> {
+
+  const program = await getProgram(provider);
+  console.log("program", program.programId);
+  const [protocolPda, _protocolBump] = await getPda(provider, 'convergence_rfq');
+  const tx = await program.rpc.initialize(new anchor.BN(feeDenominator), new anchor.BN(feeNumerator), {
+    accounts: {
+      authority: provider.wallet.publicKey,
+      protocol: protocolPda,
+      systemProgram: anchor.web3.SystemProgram.programId
+    }
+  });
+  const state = await program.account.globalState.fetch(protocolPda)
+  return { tx, state };
+}
+
+export async function getPda(provider: any, seed: string): Promise<any> {
+  const program = await getProgram(provider);
+  const [protocolPda, protocolBump] = await anchor.web3.PublicKey.findProgramAddress(
+    [Buffer.from(anchor.utils.bytes.utf8.encode(seed))],
+    program.programId
+  );
+  return [protocolPda, protocolBump];
+}
+
+export async function getProgram(provider: Provider): Program<Rfq> {
+  const programId = new anchor.web3.PublicKey(idl.metadata.address);
+  // @ts-ignoreå
+  return new anchor.Program(idl, programId, provider);
+}
+
+export const toBuffer = (x: any) => {
+  console.log("Fuck fuck", x);
+  return Buffer.from(anchor.utils.bytes.utf8.encode(x));
+}
+
+ // 1. initialize new RFQ ecosystem
+  // 2. create a new RFQ 
+  // 3. wallet A posts a new limit order in the created RFQ
+  // 4. wallet B posts a new limit order in the created RFQ
+  // 5. wallet C places a market order in the created RFQ 
+  // 6. wallet A settles
+  // 7. wallet B settles
+  // 8. wallet C settles
