9780969bb653e62f446dffc319fdd279852dc0de
Pindaroso <me@pindaroso.com>
2022-05-20 09:50:21 -0400

    Removed build flags

--- programs/rfq/src/access_controls.rs
+++ programs/rfq/src/access_controls.rs
@@ -8,21 +8,11 @@ use crate::states::*;
 /// Initialzie access control.
 ///
 /// Ensures:
-/// - Signer is Convergence DAO
 /// - Fee denominator is greater than 0
 pub fn initialize_access_control<'info>(
     _ctx: &Context<Initialize<'info>>,
     fee_denominator: u64,
 ) -> Result<()> {
-    #[cfg(feature = "devnet")]
-    let signer = _ctx.accounts.signer.key();
-    #[cfg(feature = "devnet")]
-    let dao: Pubkey = "9XDtzeAwdc8sinFAR887UijxxnjB3rXztTeQjcFUtU5y"
-        .parse()
-        .unwrap();
-    #[cfg(feature = "devnet")]
-    require!(signer == dao, ProtocolError::InvalidAuthority);
-
     require!(fee_denominator > 0, ProtocolError::InvalidFee);
 
     Ok(())
@@ -31,21 +21,16 @@ pub fn initialize_access_control<'info>(
 /// Set fee access control.
 ///
 /// Ensures:
-/// - Signer is Convergence DAO
+/// - Signer is DAO
 /// - Fee denominator is greater than 0
 pub fn set_fee_access_control<'info>(
-    _ctx: &Context<SetFee<'info>>,
+    ctx: &Context<SetFee<'info>>,
     fee_denominator: u64,
 ) -> Result<()> {
-    #[cfg(feature = "mainnet")]
-    let signer = _ctx.accounts.signer.key();
-    #[cfg(feature = "mainnet")]
-    let dao: Pubkey = "9sZmY1J1L31d6Pw2yUF3p99sob7dbSJDNpYCxUGx3AsU"
-        .parse()
-        .unwrap();
-    #[cfg(feature = "devnet")]
-    require!(signer == dao, ProtocolError::InvalidAuthority);
+    let signer = ctx.accounts.signer.key();
+    let authority = ctx.accounts.protocol.authority.key();
 
+    require!(signer == authority, ProtocolError::InvalidAuthority);
     require!(fee_denominator > 0, ProtocolError::InvalidFee);
 
     Ok(())
