c57985600765983ed2c2433466de4d8d9468cb49
Pindaroso <me@pindaroso.com>
2022-09-10 19:19:21 -0400

    Added Solita

---
+++ risk-engine/js/generated/index.ts
@@ -0,0 +1,17 @@
+import { PublicKey } from '@solana/web3.js'
+
+/**
+ * Program address
+ *
+ * @category constants
+ * @category generated
+ */
+export const PROGRAM_ADDRESS = '2DPSCUvdXw3TAWkyAt42gEYptbX8F4YYZtrEw9jYbt4K'
+
+/**
+ * Program public key
+ *
+ * @category constants
+ * @category generated
+ */
+export const PROGRAM_ID = new PublicKey(PROGRAM_ADDRESS)
---
+++ spot-instrument/js/generated/index.ts
@@ -0,0 +1,17 @@
+import { PublicKey } from '@solana/web3.js'
+
+/**
+ * Program address
+ *
+ * @category constants
+ * @category generated
+ */
+export const PROGRAM_ADDRESS = 'GxT7mn3cmaJMvX2FjeabgUQozyGUELqqfQwsjM98aFtC'
+
+/**
+ * Program public key
+ *
+ * @category constants
+ * @category generated
+ */
+export const PROGRAM_ID = new PublicKey(PROGRAM_ADDRESS)
