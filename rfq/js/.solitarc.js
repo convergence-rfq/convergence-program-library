804a9c72ecc33d145b2e73881b7a5e54b0cdcd98
Pindaroso <me@pindaroso.com>
2022-09-10 18:15:52 -0400

    Refactored and added Solita for RFQ

---
+++ rfq/js/.solitarc.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+const programDir = path.join(__dirname, "..", "program");
+const idlDir = path.join(__dirname, "idl");
+const sdkDir = path.join(__dirname, "generated");
+const binaryInstallDir = path.join(__dirname, ".crates");
+
+module.exports = {
+  idlGenerator: "shank",
+  programName: "rfq",
+  idlDir,
+  sdkDir,
+  binaryInstallDir,
+  programDir,
+};
---
+++ rfq/js/generated/index.ts
@@ -0,0 +1,17 @@
+import { PublicKey } from '@solana/web3.js'
+
+/**
+ * Program address
+ *
+ * @category constants
+ * @category generated
+ */
+export const PROGRAM_ADDRESS = 'F3DbKqFNpgE3AKtzdscoVt5fbiXmy9jNGDEhMHZyBX7t'
+
+/**
+ * Program public key
+ *
+ * @category constants
+ * @category generated
+ */
+export const PROGRAM_ID = new PublicKey(PROGRAM_ADDRESS)
