8d8b51248ff81c4643e48ef59fee28fe6d2ebc7e
strange-dv <andrii.dovzhenko.dv@gmail.com>
2023-01-06 19:51:37 +0200

    print trade settlement with hxro setup

--- tests/integration/printTradeSettlementWithHero.spec.ts
+++ tests/integration/printTradeSettlementWithHero.spec.ts
@@ -1,12 +1,35 @@
-// Outline of tests to add, also check that assets are actually exchanging hands and collateral movement
-// 1. Both parties fail to prepare and both default
-// 2. The taker prepares, but the maker defaults
-// 3. The maker prepares, but the taker defaults
-// 4. The taker prepares, the maker executes, and the maker defaults
-// 5. The taker prepares, the maker executes, and the taker defaults
-// 6. The taker prepares, the maker executes, and both default
-// 7. The taker prepares, and the maker successfully executes
-// 8. The maker prepares, the taker executes, and the maker defaults
-// 9. The maker prepares, the taker executes, and the taker defaults
-// 10. The maker prepares, the taker executes, and both default
-// 11. The maker prepares, and the taker successfully executes
+describe("RFQ HXRO instrument integration tests", () => {
+    it("Both parties fail to prepare and both default", async() => {
+
+    })
+    it("The taker prepares, but the maker defaults", async() => {
+
+    })
+    it("The maker prepares, but the taker defaults", async() => {
+
+    })
+    it("The taker prepares, the maker executes, and the maker defaults", async() => {
+
+    })
+    it("The taker prepares, the maker executes, and the taker defaults", async() => {
+
+    })
+    it("The taker prepares, the maker executes, and both default", async() => {
+
+    })
+    it("The taker prepares, and the maker successfully executes", async() => {
+
+    })
+    it("The maker prepares, the taker executes, and the maker defaults", async() => {
+
+    })
+    it("The maker prepares, the taker executes, and the taker defaults", async() => {
+
+    })
+    it("The maker prepares, the taker executes, and both default", async() => {
+
+    })
+    it("The maker prepares, and the taker successfully executes", async() => {
+
+    })
+})
