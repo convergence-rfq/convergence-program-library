010cad05f494a1a17b94d43c956e8e32ab744f96
Pindaroso <me@pindaroso.com>
2023-01-20 08:58:06 -0500

    Added @convergence-rfq/solita

--- infra/build-solita.sh
+++ infra/build-solita.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-set -x
-
 PROJDIR=$PWD
 
 function build_solita {
     yarn 
     yarn solita
     yarn prepublish
+    npm publish
 }
 
 cd $PROJDIR/rfq/js
+build_solita
+
+cd $PROJDIR/risk-engine/js
 build_solita
 
 cd $PROJDIR/spot-instrument/js
